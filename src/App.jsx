a0dfe99fac3ee9cf93facfb185ed98a9b0238551
Damian Pileckas <damianpileckas@gmail.com>
2024-08-03 03:10:07 -0300

    CartContext, Cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import ItemListContainer from './components/itemListContainer/ItemListContainer.
 import './App.css'
 import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import CartContextProvider from './components/context/CartContext.jsx'
+
 function App() {
 
 
   return (
     <>
       
+      <CartContextProvider>
+        <BrowserRouter>
+          <Navbar/> 
+          <Routes>
+            <Route path={"/"} element={<ItemListContainer/>} />
+            <Route path={"/category/:id"} element={<ItemListContainer/>}/>
+            <Route path={"/item/:id"} element={<ItemDetailContainer/>}/>
+          </Routes>
 
-      <BrowserRouter>
-        <Navbar/> 
-        <Routes>
-          <Route path={"/"} element={<ItemListContainer/>} />
-          <Route path={"/category/:id"} element={<ItemListContainer/>}/>
-          <Route path={"/item/:id"} element={<ItemDetailContainer/>}/>
-        </Routes>
-
-      </BrowserRouter>
+        </BrowserRouter>
+      </CartContextProvider>
     </>
   )
 }
--- src/components/itemCount/ItemCount.jsx
+++ src/components/itemCount/ItemCount.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
-
-const ItemCount = ({stock}) => {
+import { Link } from "react-router-dom";
+const ItemCount = ({stock, agregar}) => {
   const [count, setCount] = useState(0);
   const [itemStock, setItemStock] = useState(stock);
-  console.log("TEST 1 =>"+itemStock)
+  const [ visible, setVisible ] = useState(true);
   const increment = () => {
     if(count < itemStock){
       setCount(count + 1);
     }
   };
-  console.log("TEST 2 =>"+itemStock)
   const decrement = () => {
     if (count > 0) {
       setCount(count - 1);
     }
   };
-  console.log("TEST 3 =>"+itemStock)
   const onAdd = () => {
     console.log(itemStock) 
     if(count <= itemStock){
       setItemStock(itemStock - count);
+      agregar(count)
       setCount(1)
+      setVisible(false);
     }
     if(itemStock > 1){
       setCount(0)
@@ -31,24 +31,25 @@ const ItemCount = ({stock}) => {
     setItemStock(stock)
   }, [stock])
 
-  console.log("TEST 4 =>"+itemStock)
   return (
-    <div className="counter-container d-flex justify-content-center">
-      <div className='row w-75'>
-        <div className='col'>  
-          <div className="btn-group p-1">
-              <button type="button" className="btn btn-primary rounded-start-pill" onClick={decrement}>-</button>
-              <input type="number" className="btn btn-primary w-25" value={count} readOnly />
-              <button type="button" className="btn btn-primary rounded-end-pill" onClick={increment}>+</button>
+
+    <>
+      {visible ? 
+      <div className="counter-container d-flex justify-content-center">
+        <div className='row w-75'>
+          <div className='col'>  
+            <div className="btn-group p-1">
+                <button type="button" className="btn btn-primary rounded-start-pill" onClick={decrement}>-</button>
+                <input type="number" className="btn btn-primary w-25" value={count} readOnly />
+                <button type="button" className="btn btn-primary rounded-end-pill" onClick={increment}>+</button>
+            </div>
+            <button className="btn btn-primary" onClick={onAdd}>
+              Agregar al carrito
+            </button>
           </div>
-          <button className="btn btn-primary" onClick={onAdd}>
-            Agregar al carrito
-          </button>
         </div>
-        
-      </div>
-      
-    </div>
+      </div> : <Link to={"/cart"} className="btn btn-primary rounded-pill">Terminar Mi Compra</Link> }
+    </>
   );
 };
 
